4cd0cca303a3e2153ce01e817a962101c2a965b4
David Cortes <david.cortes.18@gmail.com>
2015-06-09 12:14:53 +0200

    Added app icon

--- Podfile
+++ Podfile
@@ -1,5 +1,7 @@
 platform :ios, '8.0'
 
+use_frameworks!
+
 pod 'AFNetworking'
 pod 'Valet'
 pod 'JVFloatLabeledTextField'
--- theThings.io-iOS/Constants.h
+++ theThings.io-iOS/Constants.h
@@ -1,4 +1,6 @@
-//#error "SETUP YOUR APP ID HERE"
+#error "SETUP YOUR APP ID HERE"
+// Create an app on http://panel.thethings.io and put here the information. Then remove the error line.
+
 #define kAppId @"YOUR_APP_ID_GOES_HERE"
 #define kThingToken @"YOUR_THING_TOKEN_GOES_HERE"
 #define kThingID @"YOUR_THING_ID_GOES_HERE"
